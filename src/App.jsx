44b4a578dea4a73e6a21bc8af55b524bca29a179
carzy-zala <mr.zala2003@gmail.com>
2024-12-08 21:45:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-500">cls</h1>
+    </>
+  );  
+}
+
+export default App;
---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import { useContex, createContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo msg",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo:(id,todo)=>{},
+  deleteTodo:(id)=>{},
+  toggleComplete:(id)=>{}
+});
+
+export const useTodo = () => {
+  return useContex(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
